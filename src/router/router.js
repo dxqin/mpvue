396f85779f6e2be1274a983a4722eddd56073534
yqqzxc <qiqi.yu@mail.geeu.tech>
2018-10-17 14:15:03 +0800

    预定订单 积分 我的账户

--- src/router/router.js
+++ src/router/router.js
@@ -55,4 +55,33 @@ module.exports = [
         enablePullDownRefresh: true
       }
     },
+    // 开始
+    {
+      path: '/pages/reserve/index',
+      config: {
+        navigationBarTitleText: '订单详情',
+        enablePullDownRefresh: false
+      }
+    },
+    {
+      path: '/pages/integral/index',
+      config: {
+        navigationBarTitleText: '积分',
+        enablePullDownRefresh: false
+      }
+    },
+    {
+      path: '/pages/my-account/index',
+      config: {
+        navigationBarTitleText: 'EBO HOTEL',
+        enablePullDownRefresh: false
+      }
+    },
+    {
+      path: '/pages/reserve-list/index',
+      config: {
+        navigationBarTitleText: 'EBO HOTEL',
+        enablePullDownRefresh: false
+      }
+    }
   ]
