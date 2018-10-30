025675dc83d4fa9873cbb94b17e6652ea3dd51ea
yqqzxc <qiqi.yu@mail.geeu.tech>
2018-10-30 17:26:43 +0800

    eee

--- src/pages/my-account/index.vue
+++ src/pages/my-account/index.vue
@@ -14,13 +14,21 @@
     </div>
     <div class="accpunt-admin flex-row jc-bet">
       <div class="admin-left">
-        <p class="flex-row jc-str">
-          <span class="c333 f20 pr20 text-overf-ell" style="max-width: 400rpx; display: inline-block">{{name}}</span>
-          <span class="cc6 f12" style="line-height: 66rpx;">{{sexArr[sex]}}</span>
-        </p>
-        <p class="c-app f12 pt20">查看及修改个人资料</p>
+        <div v-if="isNull">
+          <p class="flex-row jc-str">
+            <span class="c333 f20 pr20 text-overf-ell" style="max-width: 400rpx; display: inline-block">{{name}}</span>
+            <span class="cc6 f12" style="line-height: 66rpx;">{{sexArr[sex]}}</span>
+          </p>
+          <p class="c-app f12 pt20"  @click="lookMain">查看及修改个人资料</p>
+        </div>
+        <div v-else>
+          <p class="flex-row jc-str" style="font-size: 20px; color: #353535" @click="toLogin">
+            登录/注册
+          </p>
+          <p class="c-app f12 pt20" @click="lookMain">查看及修改个人资料</p>
+        </div>
       </div>
-      <div class="admin-right" style="background: red">
+      <div class="admin-right" style="background: #fff; box-shadow: 0 0 10rpx #ddd;">
         <img class="admin-nike-img" :src="header" alt="">
       </div>
     </div>
@@ -42,9 +50,10 @@ export default {
       name: '张三张',
       sex: 0,
       sexArr: ['男', '女'],
+      isNull: false,
       listData: [{
         text: '所有订单',
-        code: 'all_order'
+        code: 'order'
       }, {
         text: '积分',
         code: 'integral'
@@ -64,9 +73,30 @@ export default {
     }
   },
   methods: {
-    navagate: function(code) {
-      console.log(code)
-    }, ///users/user/detail
+    // navagate: function(code) {
+    //   console.log(code)
+    // },
+    lookMain() {
+      wx.navigateTo({
+        url: '../../pages/user/index'
+      })
+    },
+    toLogin() {
+      wx.redirectTo({
+        url: '../../pages/register/index'
+      })
+    },
+    navagate(url) {
+      if (url == 'invite') {
+        wx.navigateTo({
+          url: `../../pages/mine/invite`
+        })
+      } else {
+        wx.navigateTo({
+          url: `../../pages/${url}/index`
+        })
+      }
+    },
     userDetial() {
       // const hoteltestUserId = this.$wxasync.getStorage('hoteltestUserId')
       this.$wxasync.getStorage('hoteltestUserId').then((res = {}) => {
@@ -75,12 +105,18 @@ export default {
           userId: hoteltestUserId
         }
         this.$http.get('/users/user/detail', params).then((res = {}) => {
-          const { data = {} } = res;
-          const { headImageUrl = '', name = '', sex = 0 } = res
-          this.header = headImageUrl;
-          this.name = name;
-          this.sex = sex;
+          const { data = {}, code = 0 } = res;
+          if (code == 0) {
+            const { headImageUrl = '', name = '', sex = 0 } = res
+            this.header = headImageUrl;
+            this.name = name;
+            this.sex = sex;
+            this.isNull = false
+          } else {
+            this.isNull = true
+          }
         }).catch(res => {
+          this.isNull = true
           console.log(res, 'resErr')
         });
       }).catch((res = {}) => {
